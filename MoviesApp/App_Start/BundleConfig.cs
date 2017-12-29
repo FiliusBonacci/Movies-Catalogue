fecb36a75af1cff3541bd412614f4f58734750c1
Paweł Jadanowski <pjadanowski@gmail.com>
2017-12-29 20:18:29 +0100

    initial commit

--- MoviesApp/App_Start/BundleConfig.cs
+++ MoviesApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MoviesApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cosmo.css",
                       "~/Content/site.css"));
         }
     }
---
+++ MoviesApp/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesApp.Models
+{
+   
+    public class Review
+    {
+        public int ID { get; set; }
+        public int MovieID { get; set; }
+        public int UserID { get; set; }
+        public int Grade { get; set; }
+
+        public virtual Movie Movie { get; set; }
+//        public virtual User User { get; set; }
+
+    }
+}
