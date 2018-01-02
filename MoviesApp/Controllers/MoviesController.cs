71a7453c01a290d66baf325c4302d71a3b5c2723
Paweł Jadanowski <pjadanowski@gmail.com>
2018-01-02 23:46:07 +0100

    form template for movies

--- MoviesApp/Controllers/MoviesController.cs
+++ MoviesApp/Controllers/MoviesController.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoviesApp.Models;
 
 namespace MoviesApp.Controllers
 {
     public class MoviesController : Controller
     {
+        public static List<Movie> _movies = new List<Movie>
+        {
+            new Movie{ID = 1, Title = "Pierwszy", Description="lorem ipsum", ReleaseDate = DateTime.Today}
+        };
         // GET: Movies
         public ActionResult Index()
         {
+            var model = _movies;
+            return View(model);
+        }
+
+        
+        
+        public ActionResult Details(int id)
+        {
+            var movie = _movies.Single(m => m.ID == id);
+            return View(movie);
+        }
+         
+
+        public ActionResult Create()
+        {
             return View();
         }
+        
+        public ActionResult Edit(int id)
+        {
+            var movie = _movies.Single(m => m.ID == id);
+            return View(movie);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Movie movies)
+        {
+            var movie = _movies.Single(m => m.ID == id);
+            if (TryUpdateModel(movie))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+      
     }
 }
--- MoviesApp/Models/Movie.cs
+++ MoviesApp/Models/Movie.cs
@@ -8,7 +8,8 @@ namespace MoviesApp.Models
     public class Movie
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
         public DateTime ReleaseDate{ get; set; }
 
     }
